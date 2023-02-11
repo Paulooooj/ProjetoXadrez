f4c6b0187ca929b6443c30c03d7e737255988c1f
Paulo Oliveira <paulosergio_gyn@hotmail.com>
2023-02-10 23:06:05 -0300

    Criando a Posicao

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Xadrez_console
+﻿using System.Net.WebSockets;
+using tabuleiro;
+
+namespace Xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao ps = new Posicao(3, 4);
+
+
+            Console.WriteLine(ps);
         }
     }
 }
